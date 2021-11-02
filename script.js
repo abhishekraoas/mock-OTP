17938fa843a69928597caa43081adfbbc4ab9aef
Mohasin Haque <mohasinhaque23121@gmail.com>
2021-11-02 20:29:35 +0530

    Adding API

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const input = document.querySelector(".input")
+const input = document.querySelectorAll(".input")
 const firstBtn = document.querySelector("#first-btn")
 const secondBtn = document.querySelector("#second-btn")
+
+function fetchApi(){
+    fetch(url). then(response => response.json)
+    
+}
+
+firstBtn.addEventListener("click", fetchApi)
