91ea7c18c879c8f81ade61beffa23014713ddf1a
Mohasin Haque <mohasinhaque23121@gmail.com>
2021-11-03 14:42:46 +0000

    setting up api

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const input1 = document.querySelector("#input1").value;
+const input1 = document.querySelector("#input1");
 const input2 = document.querySelector("#input2")
 const firstBtn = document.querySelector("#first-btn")
 const secondBtn = document.querySelector("#second-btn")
 const outputDiv = document.querySelector("#output")
-// var text = input1.value;
-// console.log(text)
-const url = "https://otpgenerator.ishanjirety.repl.co/get-otp?name=" + input1;
+var url = "https://otpgenerator.ishanjirety.repl.co/get-otp?name=";
+var text = input1.value;
+const serverURL = url + text;
 function fetchApi(){
-    fetch(url).then(response => response.json()).then(json => outputDiv.innerText = json.otp)
+    fetch(serverURL).then(response => response.json()).then(json => outputDiv.innerText = json.otp)
     console.log("hello")
 }
 
